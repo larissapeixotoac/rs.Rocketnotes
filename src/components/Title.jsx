406434e71a293a8cb9b2639b7e1415ec74da7d26
RissaCP <larissapacavalcante@gmail.com>
2023-05-20 10:52:12 -0300

    update front end

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export function Title() {
     return (
         <div>
-            <h1 className=" font-bold text-5xl text-ORANGE text-center">Rcoket Notes</h1>
+            <h1 className=" font-bold text-5xl text-ORANGE text-center">Rocket Notes</h1>
             <p className=" text-sm text-GRAY_100 text-center mt-1">Aplicação para salvar e gerenciar seus links úteis.</p>
         </div>
     )
